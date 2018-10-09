1991e33a235b547f0e8dfcc343dc5cf423ebd8c4
meiyu si <simeiyu@huayun.com>
2018-10-09 17:58:52 +0800

    for rebase

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
  * @Date: 2018-10-08 16:06:46
  */
 // 通过CommonJS规范导入css模块
-require('./main.css');
+// require('./main.css');
 require('./main.less');
 // 通过 CommonJS 规范导入 show 函数
 const show = require('./show.js');
